591c91c5224b00f17d7b7d305539b691553f629e
igor <estudostec@hotmail.com>
2021-04-28 23:09:45 -0300

    insertTableFuncionario

---
+++ Pizzaria_Queijada/src/mainPackage/daoControllerMetodos.java
@@ -0,0 +1,42 @@
+package mainPackage;
+
+import funcionariosPackege.funcionario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
+import java.sql.SQLException;
+//import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class daoControllerMetodos {
+
+    private Connection con;
+
+    public daoControllerMetodos() {
+        this.con = new connectionFactory().getConnection();
+    }
+
+    public void gravar(funcionario funcionario) {
+        String sql = "insert into funcionario (matricula, nome, endereco, senha) values (?,?,?,?)";
+
+        try {
+            PreparedStatement stmt = con.prepareStatement(sql);
+
+            //push nos campos de funcionario
+            stmt.setInt(1, funcionario.getMatricula());
+            stmt.setString(2, funcionario.getNome());
+            stmt.setString(3, funcionario.getEndereco());
+            stmt.setInt(4, funcionario.getSenha());
+            System.out.println("Usuario ADICIONADOS no banco");
+
+            stmt.execute();
+            stmt.close();
+            con.close();
+
+            System.out.println("Gravado com sucesso");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- Pizzaria_Queijada/src/mainPackage/mainexec.java
+++ Pizzaria_Queijada/src/mainPackage/mainexec.java
@@ -7,17 +7,39 @@ public class mainexec {
 
     public static void main(String[] args) {
 
-        funcionario fc = new funcionario();
         Scanner sc = new Scanner(System.in);
-        fc.setMatricula(1);
-        fc.setSenha(1);
+        funcionario fc = new funcionario();
 
-        System.out.println("Qual a sua MATRICULA?");
+        System.out.println("Digite sua MATRICULA");
         fc.setMatricula(sc.nextInt());
-        System.out.println("Qual a sua MATRICULA?");
+
+        System.out.println("Digite seu NOME");
+        fc.setNome(sc.next());
+
+        System.out.println("Digite seu ENDERECO");
+        fc.setEndereco(sc.next());
+
+        System.out.println("Digite sua SENHA");
         fc.setSenha(sc.nextInt());
 
-        System.out.println("Sua mtricula é:" + fc.getMatricula());
-        System.out.println("Sua senha é:" + fc.getSenha());
+        daoControllerMetodos VrController = new daoControllerMetodos();
+        VrController.gravar(fc);
+
+//
+//==============================================
+//connectionDao dao = new connectionDao();
+//
+//        funcionario fc = new funcionario();
+//        Scanner sc = new Scanner(System.in);
+//        fc.setMatricula(1);
+//        fc.setSenha(1);
+//
+//        System.out.println("Qual a sua MATRICULA?");
+//        fc.setMatricula(sc.nextInt());
+//        System.out.println("Qual a sua MATRICULA?");
+//        fc.setSenha(sc.nextInt());
+//
+//        System.out.println("Sua mtricula é:" + fc.getMatricula());
+//        System.out.println("Sua senha é:" + fc.getSenha());
     }
 }
