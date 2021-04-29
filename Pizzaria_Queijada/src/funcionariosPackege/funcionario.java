ceb4ab7611871ca7ecda62f006128a54895368f8
igor <estudostec@hotmail.com>
2021-04-28 21:35:22 -0300

    metodos-criado

---
+++ Pizzaria_Queijada/src/funcionariosPackege/funcionario.java
@@ -0,0 +1,41 @@
+package funcionariosPackege;
+
+public class funcionario {
+
+    private int matricula;
+    private String nome;
+    private String endereco;
+    private int senha;
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getSenha() {
+        return senha;
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+}
