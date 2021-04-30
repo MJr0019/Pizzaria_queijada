0b2f7fbebf2f6fe11ee199e7ca0262e1c1117d78
Marcílio Júnior <marciliojunior019@hotmail.com>
2021-04-30 00:53:55 -0300

    Update Project, Merge from main

--- Pizzaria_Queijada/src/mainPackage/daoControllerMetodos.java
+++ Pizzaria_Queijada/src/mainPackage/daoControllerMetodos.java
@@ -39,4 +39,26 @@ public class daoControllerMetodos {
             throw new RuntimeException(e);
         }
     }
+
+    // new method editar
+    public void update(funcionario funcionario) {
+        String sql = "update funcionario set matricula = ? , nome = ?, endereco = ?, senha = ?";
+
+        try {
+            PreparedStatement stmt = con.prepareStatement(sql);
+
+            stmt.setInt(1, funcionario.getMatricula());
+            stmt.setString(2, funcionario.getNome());
+            stmt.setString(3, funcionario.getEndereco());
+            stmt.setInt(4, funcionario.getSenha());
+            System.out.println("Usuario Atualizados no banco");
+
+            stmt.execute();
+            stmt.close();
+            con.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
